7a0263f74776771d4438009b813d33b3b2086b4c
Nick Heisler <nickheisler63@gmail.com>
2021-06-07 19:28:17 -0500

    init

---
+++ src/impl/x86_64/boot/header.asm
@@ -0,0 +1,16 @@
+header_start:
+    dd 0xe85250d6 ; multiboot 2
+
+    dd 0          ; protected mode i386
+
+    dd header_end - header_start
+
+    ; checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+
+    ; End
+    dw 0
+    dw 0 
+    dd 8
+header_end:
